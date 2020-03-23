5166036af6ce1bf345f3993ee574e8faf0dea141
anna bateman <annacbateman@gmail.com>
2020-03-23 23:59:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,9 @@ def find_element_index(array, value_to_find)
   array.length.times do |index|
     if array[index] == value_to_find
   return index
+else "nil"
 end
+
 end
 
 def find_max_value(array)
