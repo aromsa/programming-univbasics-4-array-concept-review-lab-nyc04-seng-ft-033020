5315a3d0debb093c13340ff5a8c88a4348e4d8aa
anna bateman <annacbateman@gmail.com>
2020-03-24 00:11:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,8 +8,15 @@ nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  max = 0 
+  array.length.times do |index|
+    if array [index] > max
+      max = array [index]
 end
+end
+max
+end
+
 
 def find_min_value(array)
   # Add your solution here
